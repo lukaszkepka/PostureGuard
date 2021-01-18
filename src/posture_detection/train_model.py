49b9ae955f6821231986c146cddb618aa505b57f
lukasz.kepka <lukaszkepka95@gmail.com>
2021-01-18 23:16:03 +0100

    Simple training baseline

--- src/posture_detection/train_model.py
+++ src/posture_detection/train_model.py
@@ -1,9 +1,11 @@
 import argparse
 import os.path as path
-
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import tensorflow as tf
 import pandas as pd
 
-from annotations import Keypoints
+from annotations import Keypoints, SUPPORTED_CLASSES
 from posture_detection.preprocessing import PreProcessingPipeline, NormalizePointCoordinatesToBoundingBox, \
     KeepColumns, PointsToVectors
 
@@ -28,10 +30,28 @@ def main(args):
         PointsToVectors(starting_point_name='center')
     ])
 
-    for row_i in annotations_data_frame.iloc:
-        preprocessing_pipeline.run(annotations_data_frame.iloc[row_i[0]])
+    dataset = preprocessing_pipeline.run(annotations_data_frame)
+    # TODO: move adding category column to preprocessing pipeline
+    categories = pd.Categorical(annotations_data_frame['class'], categories=SUPPORTED_CLASSES).codes
 
-    pass
+    X_train, X_test, Y_train, Y_test = train_test_split(dataset, categories, train_size=0.9)
+    N, D = X_train.shape
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(D)),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
+    history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=1000)
+
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val loss')
+    plt.legend()
+    plt.show()
+
+    print('Train accuracy = ', model.evaluate(X_train, Y_train))
+    print('Test accuracy = ', model.evaluate(X_test, Y_test))
 
 
 if __name__ == '__main__':
